e2051d8e41bd47e18739e486245e9b3e8dce6d0e
Emma Randall <eerandall1@gmail.com>
2019-01-15 14:59:59 +0000

    default article limit of 10

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,11 +1,14 @@
 const connection = require('../db/connection');
 
 exports.sendAllArticles = (req, res, next) => {
+  const { limit = 10 } = req.params;
+
   connection('articles')
     .select('articles.username as author', 'title', 'articles.article_id', 'articles.body', 'articles.votes', 'articles.created_at', 'topic')
     .leftJoin('comments', 'comments.article_id', 'articles.article_id')
     .count('comments.comment_id as comment_count')
     .groupBy('articles.article_id')
+    .limit(limit)
     .then((articles) => {
       res.status(200).send({ articles });
     })
